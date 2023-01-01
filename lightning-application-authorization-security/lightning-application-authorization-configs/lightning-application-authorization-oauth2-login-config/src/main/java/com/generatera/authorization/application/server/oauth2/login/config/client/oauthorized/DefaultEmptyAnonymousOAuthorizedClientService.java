b57c54e34896376d90240ee09944ca08fe4fc63a
jasonj <617229004@qq.com>
2023-01-01 14:01:43 +0800

    application auth server abstract finish

--- lightning-application-authorization-security/lightning-application-authorization-configs/lightning-application-authorization-oauth2-login-config/src/main/java/com/generatera/authorization/application/server/oauth2/login/config/client/oauthorized/DefaultEmptyAnonymousOAuthorizedClientService.java
+++ lightning-application-authorization-security/lightning-application-authorization-configs/lightning-application-authorization-oauth2-login-config/src/main/java/com/generatera/authorization/application/server/oauth2/login/config/client/oauthorized/DefaultEmptyAnonymousOAuthorizedClientService.java
@@ -3,23 +3,25 @@ package com.generatera.authorization.application.server.oauth2.login.config.clie
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 不存储任何OAuthorizedClient
  */
 public class DefaultEmptyAnonymousOAuthorizedClientService implements LightningAnonymousOAuthorizedClientRepository {
     @Override
-    public <T extends OAuth2AuthorizedClient> T loadAuthorizedClient(String clientRegistrationId, String principalName) {
+    public <T extends OAuth2AuthorizedClient> T loadAuthorizedClient(String clientRegistrationId, Authentication principal, HttpServletRequest request) {
         return null;
     }
 
     @Override
-    public void saveAuthorizedClient(OAuth2AuthorizedClient authorizedClient, Authentication principal) {
+    public void saveAuthorizedClient(OAuth2AuthorizedClient authorizedClient, Authentication principal, HttpServletRequest request, HttpServletResponse response) {
         // pass
     }
 
     @Override
-    public void removeAuthorizedClient(String clientRegistrationId, String principalName) {
-
+    public void removeAuthorizedClient(String clientRegistrationId, Authentication principal, HttpServletRequest request, HttpServletResponse response) {
         // pass
     }
 }
