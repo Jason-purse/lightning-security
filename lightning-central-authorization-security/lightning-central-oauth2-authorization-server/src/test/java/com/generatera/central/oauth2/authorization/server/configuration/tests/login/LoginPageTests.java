cb33ff1245da3ddbe8524b607f5a05568c6eba79
FLJ <617229004@qq.com>
2023-03-20 14:52:48 +0800

    html 页面支持

--- lightning-central-authorization-security/lightning-central-oauth2-authorization-server/src/test/java/com/generatera/central/oauth2/authorization/server/configuration/tests/login/LoginPageTests.java
+++ lightning-central-authorization-security/lightning-central-oauth2-authorization-server/src/test/java/com/generatera/central/oauth2/authorization/server/configuration/tests/login/LoginPageTests.java
@@ -2,15 +2,21 @@ package com.generatera.central.oauth2.authorization.server.configuration.tests.l
 
 import com.generatera.central.oauth2.authorization.server.configuration.OAuth2CentralAuthorizationServerConfiguration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDriverBuilder;
 import org.springframework.web.context.WebApplicationContext;
 
+/**
+ * 中央授权服务器,进行处理 ...
+ *
+ *  所需要的无非就是,提供者配置 ...
+ *
+ */
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringJUnitWebConfig
 public class LoginPageTests {
@@ -21,6 +27,8 @@ public class LoginPageTests {
     @Import(OAuth2CentralAuthorizationServerConfiguration.class)
     @Configuration
     public static class CentralServerConfiguration {
+
+
 
     }
 
